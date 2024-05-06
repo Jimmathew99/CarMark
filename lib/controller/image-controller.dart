c85f3261cea14f2729f375f838984d633a06b9f1
Jim <jimmathew1999@gmail.com>
2024-05-06 16:27:27 +0530

    k

--- lib/controller/image-controller.dart
+++ lib/controller/image-controller.dart
@@ -1,27 +1,28 @@
 import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class CaroselController extends GetxController {
+class ImageController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    fetchImages();
+    fetchbrandImages();
+
   }
 
   // Creating RxList to store image URLs from Firestore
-  RxList<String> caroselImages = RxList<String>([]);
+  RxList<String> BrandImages = RxList<String>([]);
 
   // Method to fetch carosel images from Firestore collection
-  fetchImages() async {
+  fetchbrandImages() async {
     try {
       // Connecting to collection
       QuerySnapshot snapshot = await FirebaseFirestore.instance
-          .collection('carosel-slider')
+          .collection('brand-images')
           .get();
 
       // Check if the collection is not empty
       if (snapshot.docs.isNotEmpty) {
-        caroselImages.value = snapshot.docs
+        BrandImages.value = snapshot.docs
             .map((document) => document['image'] as String)
             .toList();
       }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carmark/controller/carosel-controller.dart';
+import 'package:carmark/controller/image-controller.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -44,26 +45,32 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  final List<String> images = ['images/Cybertruck.jpg', 'images/Model3.jpg'];
 
   //Creating object for caroselcontroller to get carosel images from firestore
   CaroselController caroselController = Get.put(CaroselController());
   int currentindex = 0;
+  ImageController imageController=Get.put(ImageController());
+  int imageindex=0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         bottomNavigationBar: BottomNavigationBar(items: [
           BottomNavigationBarItem(
+
+
               icon: Icon(
                 Icons.home_filled,
               ),
               backgroundColor: Colors.white30,
               label: "Home"),
+
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.favorite,
+
               ),
+
               backgroundColor: Colors.black,
               label: "Favorites"),
           BottomNavigationBarItem(
@@ -141,73 +148,85 @@ class _HomePageState extends State<HomePage> {
           title: Text("Home"),
           centerTitle: true,
         ),
-        body: Column(children: [
+        body: SingleChildScrollView(
+          child: Column(children: [
 
-          Obx(
-                () {
-              if (caroselController.caroselImages.isEmpty) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                return CarouselSlider.builder(
-                  itemCount: caroselController.caroselImages.length,
-                  itemBuilder:
-                      (BuildContext context, int index, int realIndex) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
+            Obx(
+                  () {
+                if (caroselController.caroselImages.isEmpty) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  return CarouselSlider.builder(
+                    itemCount: caroselController.caroselImages.length,
+                    itemBuilder:
+                        (BuildContext context, int index, int realIndex) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
 
-                          child: Image.network(
-                            caroselController.caroselImages[index],
-                            fit: BoxFit.cover,
-                          ),
-                    );
-                  },
-                  options: CarouselOptions(
-                      height: 375.h,
-                      scrollDirection: Axis.horizontal,
-                      disableCenter: true,
-                      autoPlay: true),
-                );
-              }
-            },
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(15.0).r,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-
-              children: [
-                Text(
-                  "Brands",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 12,
-              ),
-              itemCount: images.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  height: 50.h,
-                  width: 80.w,
-                  child: Image.asset(
-                    images[index],
-                    fit: BoxFit.cover,
-                  ),
-                );
+                        child: Image.network(
+                          caroselController.caroselImages[index],
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                    options: CarouselOptions(
+                        height: 375.h,
+                        scrollDirection: Axis.horizontal,
+                        disableCenter: true,
+                        autoPlay: true),
+                  );
+                }
               },
             ),
-          ),
-        ]));
+
+            Padding(
+              padding: const EdgeInsets.all(20.0).r,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+
+                children: [
+                  Text("Make",style: TextStyle(
+                    fontSize: 22,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+
+
+
+                  ),)
+                ],
+              ),
+            ),
+            Obx(
+                  () {
+                if (imageController.BrandImages.isEmpty) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  return CarouselSlider.builder(
+                    itemCount: imageController.BrandImages.length,
+                    itemBuilder:
+                        (BuildContext context, int index, int realIndex) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+
+                        child: Image.network(
+                          imageController.BrandImages[index],
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                    options: CarouselOptions(
+                        height: 60.h,
+                        scrollDirection: Axis.horizontal,
+                        disableCenter: true,),
+                  );
+                }
+              },
+            ),
+
+
+
+
+          ]),
+        ));
   }
 }

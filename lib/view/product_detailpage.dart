da440df4983df2782ada2aeb1ce6824ea6b95655
spectrumgithubtraining <githubtraining@spectrum.in>
2024-05-09 17:05:12 +0530

    trng

---
+++ lib/view/product_detailpage.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  final Map<String, dynamic> productData;
+
+  const ProductDetailScreen({Key? key, required this.productData})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Product Details'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            // Display product image
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              color: Colors.white,
+              child: Image.network(
+                productData['image'] ?? '',
+                width: MediaQuery.of(context).size.width,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20),
+            // Display product details
+            Text(
+              '${productData['brand'] ?? ''} ${productData['model'] ?? ''}',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Price: \$${productData['price'] ?? ''}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text('Fuel Type: ${productData['fueltype'] ?? ''}'),
+            SizedBox(height: 10),
+            Text('Transmission: ${productData['transmission'] ?? ''}'),
+            SizedBox(height: 10),
+
+            Text('Mileage: ${productData['mileage'] ?? ''}'),
+            SizedBox(height: 10),
+
+            Wrap(
+              spacing: 5.w,
+              children: (productData['color'] as List<dynamic> ?? [])
+                  .map<Widget>((color) {
+                return Material(
+                  borderRadius: BorderRadius.circular(20),
+                  // Same border radius as the Chip
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    // Same border radius as the Chip
+                    onTap: () {
+                      // Handle onTap event
+                    },
+                    child: Chip(
+                      label: Text(
+                        color.toString(),
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ]),
+        ));
+  }
+}
--- lib/view/product_screen.dart
+++ lib/view/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carmark/view/product_detailpage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -35,60 +36,55 @@ class _ProductScreenState extends State<ProductScreen> {
               crossAxisCount: 2,
               crossAxisSpacing: 5.0,
               mainAxisSpacing: 5.0,
-              childAspectRatio: 0.9
+              childAspectRatio: 0.9,
             ),
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
               var productData = snapshot.data!.docs[index].data() as Map<String, dynamic>;
-              return SizedBox(
-
-                child: Card(
-                  color: Colors.white,
-                  elevation: 15,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Display product image
-                      Image.network(
-                        productData['image'] ?? '',
-                        fit: BoxFit.cover,
-                      ),
-                      SizedBox(height: 10.h),
-                      // Display product details
-                      ListTile(
-                        title: Text(
-                          '${productData['brand'] ?? ''} ${productData['model'] ?? ''}',
-                          style: TextStyle(fontSize: 18.sp),
+              return GestureDetector(
+                onTap: () {
+                  // Navigate to detail page with product details
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProductDetailScreen(productData: productData),
+                    ),
+                  );
+                },
+                child: SizedBox(
+                  child: Card(
+                    color: Colors.white,
+                    elevation: 15,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Display product image
+                        Image.network(
+                          productData['image'] ?? '',
+                          fit: BoxFit.cover,
                         ),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            //Text('Fuel Type: ${productData['fueltype'] ?? ''}'),
-                            //Text('Transmission: ${productData['transmission'] ?? ''}'),
-                           // Text('Mileage: ${productData['mileage'] ?? ''}'),
-                            Text('Price: \$${productData['price'] ?? ''}'),
-                            // Displaying colors as chips
-                            // Wrap(
-                            //   spacing: 5.w,
-                            //   children: (productData['color'] as List<dynamic> ?? []).map<Widget>((color) {
-                            //     return Chip(
-                            //       label: Text(
-                            //         color.toString(),
-                            //         style: TextStyle(fontSize: 14.sp),
-                            //       ),
-                            //       backgroundColor: Colors.blue,
-                            //     );
-                            //   }).toList(),
-                            // ),
-                          ],
+                        SizedBox(height: 10.h),
+                        // Display product details
+                        ListTile(
+                          title: Text(
+                            '${productData['brand'] ?? ''} ${productData['model'] ?? ''}',
+                            style: TextStyle(fontSize: 18.sp),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Price: \$${productData['price'] ?? ''}'),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               );
             },
           );
+
         },
       ),
     );

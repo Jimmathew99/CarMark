7e83029e9c755a4cb28b192e17efaa6c90becb12
Jim <jimmathew1999@gmail.com>
2024-05-09 07:51:51 +0530

    k

--- lib/view/product_screen.dart
+++ lib/view/product_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProductScreen extends StatefulWidget {
-  const ProductScreen({super.key});
+  const ProductScreen({Key? key}) : super(key: key);
 
   @override
   State<ProductScreen> createState() => _ProductScreenState();
@@ -15,41 +16,80 @@ class _ProductScreenState extends State<ProductScreen> {
       appBar: AppBar(
         title: Text("Products"),
         centerTitle: true,
-        leading: GestureDetector(
-          onTap: () {
-            // Navigate back
-          },
-          child: Icon(Icons.arrow_back),
-        ),
+
       ),
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-              padding: const EdgeInsets.all(15.0).r,
-              child: Card(
-                elevation: 15,
-                color: Colors.black,
-                child: Container(
-                  width: 125.w,
-                  height: 210.h,
-                ),
-              )),
-          Padding(
-            padding: const EdgeInsets.all(15.0).r,
-            child: Card(
-              elevation: 15,
-              child: Container(
-                width: 125.w,
-                height: 210.h,
-                color: Colors.blue,
-              ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('products').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text('No products found.'));
+          }
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 5.0,
+                mainAxisSpacing: 5.0,
+                childAspectRatio: 0.9
             ),
-          ),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var productData = snapshot.data!.docs[index].data() as Map<String, dynamic>;
+              return SizedBox(
 
-
-        ],
+                child: Card(
+                  color: Colors.white,
+                  elevation: 15,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Display product image
+                      Image.network(
+                        productData['image'] ?? '',
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 10.h),
+                      // Display product details
+                      ListTile(
+                        title: Text(
+                          '${productData['brand'] ?? ''} ${productData['model'] ?? ''}',
+                          style: TextStyle(fontSize: 18.sp),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            //Text('Fuel Type: ${productData['fueltype'] ?? ''}'),
+                            //Text('Transmission: ${productData['transmission'] ?? ''}'),
+                            // Text('Mileage: ${productData['mileage'] ?? ''}'),
+                            Text('Price: \$${productData['price'] ?? ''}'),
+                            // Displaying colors as chips
+                            // Wrap(
+                            //   spacing: 5.w,
+                            //   children: (productData['color'] as List<dynamic> ?? []).map<Widget>((color) {
+                            //     return Chip(
+                            //       label: Text(
+                            //         color.toString(),
+                            //         style: TextStyle(fontSize: 14.sp),
+                            //       ),
+                            //       backgroundColor: Colors.blue,
+                            //     );
+                            //   }).toList(),
+                            // ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
